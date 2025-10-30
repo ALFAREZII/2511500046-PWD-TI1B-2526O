4c5532e1cd015ecbec5038562d22b9997762ae5b
Muhammad A;farezi <2511500046@mahasiswa.atmaluhur.ac.id>
2025-10-30 08:01:34 +0700

    penambahan PHP

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -61,3 +61,90 @@ const ANGKATAN = 2025;
 echo "Kampus: " . KAMPUS . "<br>";
 echo "Angkatan: " . ANGKATAN;
 ?>
+
+<?php
+// Ini komentar 1 baris
+# Ini juga komentar 1 baris
+/*
+Ini komentar
+lebih dari satu baris
+*/
+?>
+
+<?php
+$a = 10;
+$b = 3;
+echo $a + $b;
+echo $a % $b;
+?>
+
+<?php
+$a = 100;
+$b = "100";
+$c = 0;
+$d = false;
+echo "<h3>Perbandingan == dan ===</h3>";
+echo "\$a == \$b : "; var_dump($a == $b);
+echo "\$a === \$b : "; var_dump($a === $b);
+echo "\$c == \$d : "; var_dump($c == $d);
+echo "\$c === \$d : "; var_dump($c === $d);
+?>
+
+<?php
+$nilai = 80;
+if ($nilai >= 90) {
+echo "A";
+} elseif ($nilai >= 80) {
+echo "B";
+} else {
+echo "C";
+}
+?>
+
+<?php
+$hari = "Senin";
+switch ($hari) {
+case "Senin": echo "Awal Minggu!"; break;
+case "Jumat": echo "Hampir weekend!"; break;
+default: echo "Hari biasa.";
+}
+?>
+
+<?php
+$hobi = ["Coding", "Memasak", "Musik"];
+foreach ($hobi as $item) {
+echo "Hobi: $item <br>";
+}
+?>
+
+<?php
+$hobi = ["Coding", "Memasak", "Musik", "Membaca", "Traveling"];
+echo "<h3>Daftar Hobi Saya:</h3>";
+for ($i = 0; $i < count($hobi); $i++) {
+echo ($i + 1) . ". " . $hobi[$i] . "<br>";
+}
+
+echo "<hr>";
+echo "<h4>Hasil print_r():</h4>";
+echo "<pre>";
+print_r($hobi);
+echo "</pre>";
+echo "<h4>Hasil var_dump():</h4>";
+echo "<pre>";
+var_dump($hobi);
+echo "</pre>";
+?>
+
+<?php
+for ($i=1; $i<=5; $i++) {
+echo "Perulangan ke-$i <br>";
+}
+?>
+
+<?php
+$i = 1;
+do {
+echo "Iterasi ke-$i<br>";
+$i++;
+} while (1 == 1);
+?>
