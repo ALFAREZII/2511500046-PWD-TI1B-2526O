b42e0d556371e4a8cb0e87582aea9f91c1f60256
Muhammad A;farezi <2511500046@mahasiswa.atmaluhur.ac.id>
2025-11-13 11:39:48 +0700

    styling

--- pertemuan-08/index.php
+++ pertemuan-08/index.php
@@ -38,7 +38,7 @@ endif;
         <li><a href="#home">Beranda</a></li>
         <li><a href="#about">Tentang</a></li>
         <li><a href="#contact">Kontak</a></li>
-        <li><a href="#EntryDataMahasiswa">Entry Data Mahasiswa</a></li>
+        <li><a href="#EntryDataMahasiswa"></a></li>
       </ul>
     </nav>
   </header>
