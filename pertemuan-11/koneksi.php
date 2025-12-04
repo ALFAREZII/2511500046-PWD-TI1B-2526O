6711e5306c12d6c070859f976714dc327dc6d36c
Muhammad A;farezi <2511500046@mahasiswa.atmaluhur.ac.id>
2025-12-04 10:47:32 +0700

    asas

--- pertemuan-11/koneksi.php
+++ pertemuan-11/koneksi.php
@@ -4,7 +4,7 @@ $user = "root";
 $pass = "";
 $db   = "db__pwd2025";
 
-$conn = mysqli_connect($host, $user, $pass, $db)
+$conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
     die("koneksi gagal: " . mysqli_connect_erorr());
