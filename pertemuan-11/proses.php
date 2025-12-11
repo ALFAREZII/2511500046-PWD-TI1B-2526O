9dda80a05c2bcf153d39c1f9c51532718a8985ec
Muhammad A;farezi <2511500046@mahasiswa.atmaluhur.ac.id>
2025-12-11 11:29:28 +0700

    validasi

--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -14,6 +14,34 @@ $arrContact = [
   $email = bersihkan($_POST['txtEmail'] ?? '');
   $pesan = bersihkan($_POST['txtPesan'] ?? '');
 ];
+
+$errors - [];
+
+if ($nama === '') {
+    $errors[] = 'Nama wajib diisi.';
+}
+
+if ($email === '') {
+    $errors[] = 'Email wajib diisi.';
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors[] = 'Format e-mail tidak valid.';
+}
+
+if ($pesan === '') {
+    $errors[] = 'Pesan wajib diisi.';
+}
+
+if (!empty($errors)) {
+    $_SESSION['old'] = [
+        'nama'  => $nama,
+        'email' => $email,
+        'pesan' => $pesan,
+    ];
+
+    $_SESSION['flash_error'] = implode('<br>', $errors);
+    redirect_ke('index.php#contact');
+}
+
 $_SESSION["contact"] = $arrContact;
 
 $arrBiodata = [
