cbe568ff6d8ec10f3e84a14e11bd2223d171f4cf
Muhammad A;farezi <2511500046@mahasiswa.atmaluhur.ac.id>
2025-12-11 12:51:39 +0700

    ee

--- pertemuan-11/localhost.sql
+++ pertemuan-11/localhost.sql
@@ -31,6 +31,7 @@ USE `db_pwd2025`;
 --
 
 CREATE TABLE `tbl_tamu` (
+  `No` int(11) NOT NULL,
   `cid` int(11) NOT NULL,
   `cnama` varchar(100) COLLATE utf16_bin DEFAULT NULL,
   `cemail` varchar(100) COLLATE utf16_bin DEFAULT NULL,
