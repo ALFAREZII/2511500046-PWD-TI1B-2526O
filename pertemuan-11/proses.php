59dc3974fc2033cae78e8fb960c56e040bc8b352
Muhammad A;farezi <2511500046@mahasiswa.atmaluhur.ac.id>
2025-12-11 12:25:06 +0700

    breh

--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -44,7 +44,7 @@ if (!empty($errors)) {
 }
 
 $sql = "INSERT INTO tbl_tamu (cnama, cemail, cpesan) VALUES (?, ?, ?)";
-$stmt = mysqli_prepare($con, $sql);
+$stmt = mysqli_prepare($conn, $sql);
 
 if (!$stmt) {
     $_SESSION['flash_error'] = 'Terjadi kesalahan sistem (prepare gagal).';
