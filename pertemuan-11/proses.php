de5e84deae1358d7598f0131a1f6a3c7a5972001
Muhammad A;farezi <2511500046@mahasiswa.atmaluhur.ac.id>
2025-12-11 11:40:50 +0700

    fix

--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -72,17 +72,6 @@ mysqli_stmt_close($stmt);
 
 $arrContact = []
 
-  "nim" => $_POST["txtNim"] ?? "",
-  "nama" => $_POST["txtNmLengkap"] ?? "",
-  "tempat" => $_POST["txtT4Lhr"] ?? "",
-  "tanggal" => $_POST["txtTglLhr"] ?? "",
-  "hobi" => $_POST["txtHobi"] ?? "",
-  "pasangan" => $_POST["txtPasangan"] ?? "",
-  "pekerjaan" => $_POST["txtKerja"] ?? "",
-  "ortu" => $_POST["txtNmOrtu"] ?? "",
-  "kakak" => $_POST["txtNmKakak"] ?? "",
-  "adik" => $_POST["txtNmAdik"] ?? ""
-
 $_SESSION["biodata"] = $arrBiodata;
 
 header("location: index.php#about");
