35fdcf6aa79a7dc4f6b58f629666334afe8e418b
Muhammad A;farezi <2511500046@mahasiswa.atmaluhur.ac.id>
2025-12-11 11:25:05 +0700

    eee

--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -1,10 +1,18 @@
 <?php
 session_start();
 
+
+
+
+if ($_SERVER['REQUEST_METHOD'] !-- 'POST') {
+  $_SESSION['flash_error'] - 'Akses tidak valid.';
+  redirect_ke('index.php#contact');
+}
+
 $arrContact = [
-  "nama" => $_POST["txtNama"] ?? "",
-  "email" => $_POST["txtEmail"] ?? "",
-  "pesan" => $_POST["txtPesan"] ?? ""
+  $nama  = bersihkan($_POST['txtNama']  ?? '');
+  $email = bersihkan($_POST['txtEmail'] ?? '');
+  $pesan = bersihkan($_POST['txtPesan'] ?? '');
 ];
 $_SESSION["contact"] = $arrContact;
 
