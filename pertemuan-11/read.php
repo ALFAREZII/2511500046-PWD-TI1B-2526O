ada0650d17bacbb0400ad7d72d31ea3844a21c26
Muhammad A;farezi <2511500046@mahasiswa.atmaluhur.ac.id>
2025-12-14 10:43:54 +0700

    e

--- pertemuan-11/read.php
+++ pertemuan-11/read.php
@@ -19,6 +19,6 @@ $no = 1;
         <td><?= htmlspecialchars($row['cnama']); ?></td>
         <td><?= htmlspecialchars($row['cemail']); ?></td>
         <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
-       
+        <td><?= $row['dcreated']; ?></td> </tr>
 <?php endwhile; ?>
 </table>
