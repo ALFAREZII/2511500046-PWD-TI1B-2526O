2bb818f90efac0060d1f3b85ae88a15ff6cbc96b
Muhammad A;farezi <2511500046@mahasiswa.atmaluhur.ac.id>
2025-12-14 10:41:35 +0700

    Eee

--- pertemuan-11/tbl_tamu.sql
+++ pertemuan-11/tbl_tamu.sql
@@ -1,6 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 11 Des 2025 pada 09.48
+-- Versi server: 5.7.33
+-- Versi PHP: 7.4.19
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pwd2025`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_tamu`
+--
 
 CREATE TABLE `tbl_tamu` (
   `cid` int(11) NOT NULL,
@@ -10,11 +35,33 @@ CREATE TABLE `tbl_tamu` (
   `dcreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `tbl_tamu`
+--
+
 INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`, `dcreated`) VALUES
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_tamu`
+--
 ALTER TABLE `tbl_tamu`
   ADD PRIMARY KEY (`cid`);
 
-  ALTER TABLE `tbl_tamu`
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
   MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
